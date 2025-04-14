3397684312e6b7aac76156848cf00f498ab29d8d
wesleyfernandocabrera <wesley.cabrera@hotmail.com>
2025-04-14 16:13:22 +0000

    ok

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -12,11 +12,7 @@
     <title>Analytics - Admin Toolkit</title>
     <meta name="description"
         content="Admin Toolkit is a modern admin dashboard template based on Tailwindcss. It comes with a variety of useful ui components and pre-built pages" />
-    <link rel="preconnect" href="https://fonts.googleapis.com" />
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
-    <link
-        href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,500;1,600;1,700&display=swap"
-        rel="stylesheet" />
+
 
     @vite(['resources/scss/app.scss', 'resources/js/app.js'])
 
