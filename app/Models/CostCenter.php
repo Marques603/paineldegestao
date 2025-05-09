2a62faf24f2807a55b54b9e0f6a8d0aecf9cb4ac
wesleyfernandocabrera <wesley.cabrera@hotmail.com>
2025-05-09 20:34:30 +0000

    ok

--- app/Models/CostCenter.php
+++ app/Models/CostCenter.php
@@ -9,9 +9,14 @@ class CostCenter extends Model
 {
     use HasFactory;
 
-    protected $table = 'cost_center';
+    // Definindo os campos que podem ser preenchidos em massa
+    protected $fillable = [
+        'name',       // Nome do centro de custo
+        'code',       // Código do centro de custo
+        'description' // Descrição do centro de custo
+    ];
 
-    // Relacionamento muitos-para-muitos com Sector
+    // Definindo a relação com os setores
     public function sectors()
     {
         return $this->belongsToMany(Sector::class, 'cost_center_sector');
