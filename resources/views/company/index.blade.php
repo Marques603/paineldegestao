48c8b7e598eb685b946ced6360fd1d1b4c005a74
wesleyfernandocabrera <wesley.cabrera@hotmail.com>
2025-05-20 12:57:56 +0000

    ok

--- resources/views/company/index.blade.php
+++ resources/views/company/index.blade.php
@@ -86,7 +86,7 @@
                             <td>{{ $company->name }}</td>
                             <td>{{ $company->corporate_name }}</td>
                             <td>{{ $company->cnpj }}</td>
-                            <td>{{ $company->name }}</td>
+                            <td>{{ $company->users->first()?->name ?? '—' }}</td>
                             <td>
                                 @if($company->status)
                                     <div class="badge badge-soft-success">Ativa</div>
