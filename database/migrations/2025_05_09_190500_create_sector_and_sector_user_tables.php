403e9784f1a0508dc1ff450e8f9d2f8d61b906cc
wesleyfernandocabrera <wesley.cabrera@hotmail.com>
2025-05-29 19:27:34 +0000

    ok

--- database/migrations/2025_05_09_190500_create_sector_and_sector_user_tables.php
+++ database/migrations/2025_05_09_190500_create_sector_and_sector_user_tables.php
@@ -11,7 +11,7 @@ return new class extends Migration {
         Schema::create('sector', function (Blueprint $table) {
             $table->id();
             $table->string('name');
-            $table->string('description')->nullable();
+            $table->string('acronym')->nullable();
             $table->boolean('status')->default(1); // 1 = ativo, 0 = inativo
             $table->timestamps();
             $table->softDeletes(); // Caso deseje restaurar depois
